4d69dd865eb151701d2dfcb0a3db9218e9c3a273
akshat <akshatjangid14@gmail.com>
2025-03-03 15:54:56 +0530

    bugs removed

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,17 +1,16 @@
 import { WebSocketServer } from "ws";
-import pkg from "@repo/db/client";
-const { client } = pkg;
+import { client } from "@repo/db/client";
 
 const server = new WebSocketServer({
-  port: 3001,
+    port: 3001
 });
 
 server.on("connection", async (socket) => {
-  client.user.create({
-    data: {
-      username: Math.random().toString(),
-      password: Math.random().toString(),
-    },
-  });
-  socket.send("Hi there you are connected to the server");
-});
+    client.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    socket.send("Hi there you are connected to the server");
+})
